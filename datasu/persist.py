745568164f95d3692685e82a404dea36a9ee7029
root <alexander.ostrikov@gmail.com>
2015-12-24 15:44:13 +0200

    fix persist

--- datasu/persist.py
+++ datasu/persist.py
@@ -7,8 +7,8 @@ from os import listdir
 from os.path import isfile, join
 
 
-def persist_variables(variables, path='persisted_vars'):
-    if not isinstance(variables,list):
+def persist_variables(variables, vars_dict=globals() ,path='persisted_vars'):
+    if not isinstance(variables, list):
         variables = [variables]
 
     if not os.path.exists(path):
@@ -17,14 +17,14 @@ def persist_variables(variables, path='persisted_vars'):
     for v in variables:
         var_path = '%s/%s' % (path, v)
         print 'dumping %s to %s' % (v,var_path)
-        jb.dump(globals()[v], filename=var_path, compress=True)
+        jb.dump(vars_dict[v], filename=var_path, compress=True)
 
 
-def load_variables(path='persisted_vars', variables=[]):
+def load_variables(vars_dict=globals(), path='persisted_vars',variables=[]):
     files = [f for f in listdir(path) if isfile(join(path, f))]
     print files
     for v in files:
         if len(variables) == 0 or v in variables:
-            globals()[v] = jb.load(path+'/'+v)
+            vars_dict[v] = jb.load(path+'/'+v)
             print 'loaded %s' % v
 
