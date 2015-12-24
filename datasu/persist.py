174f2750114befb94711fa0143214d88bab4b226
root <alexander.ostrikov@gmail.com>
2015-12-24 16:38:39 +0200

    make persist automatically find and load vars

--- datasu/persist.py
+++ datasu/persist.py
@@ -2,12 +2,18 @@ __author__ = 'sasha'
 
 
 import joblib as jb
+import inspect
 import os
 from os import listdir
 from os.path import isfile, join
 
 
-def persist_variables(variables, vars_dict=globals(), path='persisted_vars'):
+def persist_variables(variables, path='persisted_vars'):
+
+    frm = inspect.stack()[1]
+    mod = inspect.getmodule(frm[0])
+    vars_dict = mod.globals()
+
     if not isinstance(variables, list):
         variables = [variables]
 
@@ -21,11 +27,16 @@ def persist_variables(variables, vars_dict=globals(), path='persisted_vars'):
 
 
 def load_variables(path='persisted_vars', variables=[]):
+
+    frm = inspect.stack()[1]
+    mod = inspect.getmodule(frm[0])
+
     files = [f for f in listdir(path) if isfile(join(path, f))]
     print files
     for vn in files:
         if len(variables) == 0 or vn in variables:
             vv = jb.load(path+'/'+vn)
-            exec("global {0}; {0}={1}".format(vn, vv))
+            # exec("global {0}; {0}={1}".format(vn, vv))
+            exec("mod.{0}={1}".format(vn, vv))
             print 'loaded %s' % vn
 
