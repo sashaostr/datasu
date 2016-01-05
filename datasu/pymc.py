a50b2a55a7956eca5c64a2efcd842a506995487d
sostrikov <Sasha.Ostrikov@verint.com>
2016-01-05 16:27:19 +0200

    add mcmc to dataframe

---
+++ datasu/pymc.py
@@ -0,0 +1,22 @@
+__author__ = 'Alexander Ostrikov <alexander.ostrikov@gmail.com'
+
+import pandas as pd
+import numpy as np
+
+
+def mcmc_traces_to_dataframe(mcmc):
+    def get_data(trace):
+        shape = trace.shape
+        if len(shape) == 1:
+            return trace
+        elif len(shape) == 2 and shape[1] == 1:
+            return trace[:, 0]
+        elif len(shape) == 2:
+            return trace.tolist()
+        else:
+            return np.NaN
+
+    df1 = pd.DataFrame(data=[get_data(s.trace.gettrace()) for s in mcmc.variables])
+    df2 = df1.T
+    df2.columns = [s.trace.name for s in mcmc.variables]
+    return df2
